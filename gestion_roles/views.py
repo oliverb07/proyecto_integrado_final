8a7f0c4c78adaf143802864a748a122f27668c69
oliverb07 <oliverbriones840@gmail.com>
2025-11-24 19:54:14 -0300

    ajustes final

--- gestion_roles/views.py
+++ gestion_roles/views.py
@@ -39,6 +39,8 @@ def login_view(request):
             request.session['pending_user_id'] = user.id
             request.session['otp_code'] = otp
 
+            # Mostrar OTP directamente en la pantalla (para Render)
+            messages.success(request, f"Tu código OTP es: {otp}")
 
             return redirect('gestion_roles:verificar_otp') 
         else:
