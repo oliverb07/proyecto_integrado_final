c13f044d16f8f6b6476c9335d3621700b0b2ca3c
oliverb07 <oliverbriones840@gmail.com>
2025-11-24 22:38:05 -0300

    Solucion eliminacion 1

--- neonatos/views.py
+++ neonatos/views.py
@@ -91,7 +91,7 @@ class MadreCreateView(CreateView):
 
     def get_success_url(self):
         # no se usa por el redirect inmediato
-        return reverse_lazy("neonatos:madre_list", args=[self.object.pk])
+        return reverse_lazy("neonatos:madre_detail", args=[self.object.pk])
     
 @method_decorator([login_required, matrona_required], name='dispatch')
 class MadreUpdateView(UpdateView):
@@ -156,7 +156,7 @@ class PartoCreateView(CreateView):
         
 
     def get_success_url(self):
-        return reverse_lazy("neonatos:madre_list", args=[self.object.madre.pk])
+        return reverse_lazy("neonatos:madre_detail", args=[self.object.madre.pk])
     
 @method_decorator([login_required, matrona_required], name='dispatch')   
 # === PARTO: editar y eliminar ===
@@ -273,7 +273,7 @@ class RNDeleteView(DeleteView):
 
     def get_success_url(self):
         # Volver al detalle de la madre tras eliminar
-        return reverse_lazy("neonatos:madre_list", args=[self.object.parto.madre.pk])
+        return reverse_lazy("neonatos:madre_detail", args=[self.object.parto.madre.pk])
 
 @method_decorator([login_required, matrona_required], name='dispatch')
 class RecienNacidoDetailView(DetailView):
