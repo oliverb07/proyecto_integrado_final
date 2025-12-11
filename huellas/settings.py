13d656ec2d660aec0baec7fedb174870a7d898f9
oliverb07 <oliverbriones840@gmail.com>
2025-12-11 01:06:53 -0300

    Correo con brevee

--- huellas/settings.py
+++ huellas/settings.py
@@ -173,8 +173,9 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-relay.brevo.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")  # CORREO EMISOR
 EMAIL_HOST_PASSWORD =  config("EMAIL_HOST_PASSWORD") # CONTRASEÑA DE APLICACIÓN
